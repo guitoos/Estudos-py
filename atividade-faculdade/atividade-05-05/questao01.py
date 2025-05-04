2887f846924804b878e7316781acf5fffb0944c8
guitoos <guilhermeoss2004@gmail.com>
2025-05-04 20:58:14 -0300

    questão 01

---
+++ atividade-faculdade/atividade-05-05/questao01.py
@@ -0,0 +1,6 @@
+ #Questão 1
+
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+area = (base * altura) / 2
+print(f"Área do triângulo: {area}")
